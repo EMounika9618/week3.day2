62bb4ee287b680fc370e205eba1e825dc01e2b25
mounika <105705466+EMounika9618@users.noreply.github.com>
2022-06-04 20:56:23 +0530

    Add files via upload

---
+++ MultiLanguage.java
@@ -0,0 +1,10 @@
+package week3.Day2;
+
+public abstract class MultiLanguage implements Testtool
+{
+public abstract void Python();
+	
+	public void ruby() {
+		System.out.println("Abstact class is ruby method");
+	}
+}
