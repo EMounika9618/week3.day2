c1f476f79ea012e0bfdc89b2750f80a7a87e61d4
mounika <105705466+EMounika9618@users.noreply.github.com>
2022-06-04 20:56:39 +0530

    Add files via upload

---
+++ RemoveDuplicates.java
@@ -0,0 +1,19 @@
+package week3.Day2;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class RemoveDuplicates {
+	public static void main(String arg[]) {
+		String text = "We learn java basics in java week1";
+
+		char[] allChar = text.toCharArray();
+		Set<Character> sl = new LinkedHashSet<Character>();
+		
+		for(int i=0; i<allChar.length; i++) {
+			if (sl.add(allChar[i])) {
+				System.out.print(allChar[i]);	
+			}	
+		}
+	}
+}
